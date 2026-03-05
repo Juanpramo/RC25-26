f76288c52ed5114b43be8c710d06f63368ec6e24
RanaMistica <juanpramos.cmi16@gmail.com>
2026-03-05 09:05:25 +0100

    Refactor invertir function and update comments

--- invertir.pl
+++ invertir.pl
@@ -14,13 +14,4 @@ Método resolución:
 */
 
 invertir([], []).
-/*
-invertir([Cab|Resto], ListaR):-
-	last(ListaR, Cab),
-	proper_length(ListaR, L),
-	LR is L - 1,
-	nth0((LR), ListaR, _, R),
-	invertir(Resto, R).
-*/
-
 invertir([Cab|Resto], R2):- invertir(Resto, R), append(R, [Cab], R2).
